9d2afebd3c0e6a7aaf793c505e87082b73c0d7f8
Bogdan <52568970+Bogdan1packo@users.noreply.github.com>
2021-03-24 13:45:30 +0100

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,8 @@
+user_input = input() #  '_XO__X___'
+
+
+print('---------')
+print('|' + ' ' + (user_input[0]) + ' ' + (user_input[1]) + ' ' + (user_input[2]) + ' ' + '|')
+print('|' + ' ' + (user_input[3]) + ' ' + (user_input[4]) + ' ' + (user_input[5]) + ' ' + '|')
+print('|' + ' ' + (user_input[6]) + ' ' + (user_input[7]) + ' ' + (user_input[8]) + ' ' + '|')
+print('---------')
